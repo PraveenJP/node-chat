63af214a22eddf39662a7dcdc204378fe5b9c3ec
PraveenJP <roadizpraveen@gmail.com>
2016-04-02 22:32:03 +0530

    connect to socket.io

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection',function(){
+   console.log('User is connected to socket.io'); 
+});
 
 http.listen(PORT, function(err){
    if(err){
