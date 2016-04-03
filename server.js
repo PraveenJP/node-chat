8971b0a36e480b26f2acde554bec2a2f077a7491
PraveenJP <roadizpraveen@gmail.com>
2016-04-03 20:32:53 +0530

    added currentusers

--- server.js
+++ server.js
@@ -10,6 +10,27 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Sends current users
+function sendCurrentUsers(socket){
+    var info = clientInfo[socket.id];
+    var users = [];
+    if(typeof info === 'undefined'){
+        return;
+    }
+    Object.keys(clientInfo).forEach(function(socketId){
+        var userInfo = clientInfo[socketId];
+        if(info.room === userInfo.room){
+            users.push(userInfo.name);
+        }
+    });
+    
+    socket.emit('message',{
+       name: 'System',
+       text: 'Current users:' + users.join(', '),
+       timestamp: moment.valueOf() 
+    });
+}
+
 // Server Chat connection
 io.on('connection',function(socket){
    console.log('User is connected to socket.io');
@@ -39,9 +60,17 @@ io.on('connection',function(socket){
    
    socket.on('message', function(message){
       console.log('Message Received: '+message.text);
-      message.timestamp = moment().valueOf();
-      //socket.broadcast.emit('message', message); // send message to all sender except sender
-      io.to(clientInfo[socket.id].room).emit('message', message); // send message to all users include sender
+      
+      if(message.text === '@currentUsers'){
+          sendCurrentUsers(socket);
+      }else{
+          message.timestamp = moment.valueOf();
+          io.to(clientInfo[socket.id].room).emit('message',message);
+      }
+      
+      // message.timestamp = moment().valueOf();
+      // socket.broadcast.emit('message', message); // send message to all sender except sender
+      // io.to(clientInfo[socket.id].room).emit('message', message); // send message to all users include sender
    });
    
    /*socket.emit('message',{
